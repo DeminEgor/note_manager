8bfcdbdc413b6ca2437c936333f66d532006fce5
DeminEgor <egor.demunn@yandex.ru>
2025-01-17 00:31:35 +0800

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,10 @@
+username = "Egor"
+title = "Вводный урок"
+content = "Просмотреть и изкучить вводную лекцию"
+statis = "New"
+created_data = '10-11-2024'
+issue_date = '10-12-2024'
+print('Название заметки:', title)
+print('Описание заметки:',content )
+print('Имя пользователя:', username)
+print('Срок окончания:',issue_date)
